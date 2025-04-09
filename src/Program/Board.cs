56a323e68c528aa6399d8520363af104fbe36955
Tiago <tiago.garrido@correo.ucu.edu.uy>
2025-04-09 20:52:19 -0300

    Clase Board

--- src/Program/Board.cs
+++ src/Program/Board.cs
@@ -4,14 +4,13 @@ namespace Ucu.Poo.GameOfLife;
 
 public class Board
 {
-    private bool[,] gameBoard = new bool[,] { { true, false, false }, {false, true, true}, {true, true, false} };
+    private bool[,] gameBoard = new bool[,] { { true, false, false }, {false, true, true}, {true, true, false}, {true, true, true} };
 
     public void Generacion()
     {
         int boardWidth = gameBoard.GetLength(9);
         int boardHeight = gameBoard.GetLength(9);
         bool[,] cloneboard = new bool[boardWidth, boardHeight];
-        //bool estaViva = gameBoard[x, y];
         
         for (int x = 0; x < boardWidth; x++)
         {
