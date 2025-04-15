f4583aecc4f5c66a49243918ee18f6935aa3031d
Tiago <tiago.garrido@correo.ucu.edu.uy>
2025-04-15 13:18:54 -0300

    Correccion en el Main de Board

--- src/Library/Board.cs
+++ src/Library/Board.cs
@@ -1,57 +1,27 @@
 using System;
-namespace Library;
+
+namespace Ucu.Poo.GameOfLife;
 
 public class Board
 {
-    private bool[,] gameBoard = new bool[,]
-        { { true, false, false }, { false, true, true }, { true, true, false }, { true, true, true } };
-
-    public void Generacion()
+    private bool[,] cells;
+    public void TamañoBoard()
     {
-        int boardWidth = gameBoard.GetLength(9);
-        int boardHeight = gameBoard.GetLength(9);
-        bool[,] cloneboard = new bool[boardWidth, boardHeight];
+        int anchoBoard = cells.GetLength(1);
+        int largoBoard = cells.GetLength(0);
+    }
+    public Board(int anchoBoard, int largoBoard)
+    {
+        cells = new bool[anchoBoard, largoBoard];
+    }
+    public bool GetValor(int x, int y)
+    {
+        return cells[x, y];
+    }
 
-        for (int x = 0; x < boardWidth; x++)
-        {
-            for (int y = 0; y < boardHeight; y++)
-            {
-                int aliveNeighbors = 0;
-                for (int i = x - 1; i <= x + 1; i++)
-                {
-                    for (int j = y - 1; j <= y + 1; j++)
-                    {
-                        if (i >= 0 && i < boardWidth && j >= 0 && j < boardHeight && gameBoard[i, j])
-                        {
-                            aliveNeighbors++;
-                        }
-                    }
-                }
-
-                if (gameBoard[x, y])
-                {
-                    aliveNeighbors--;
-                }
-
-                if (gameBoard[x, y] && aliveNeighbors < 2)
-                {
-                    cloneboard[x, y] = false;
-                }
-                else if (gameBoard[x, y] && aliveNeighbors > 3)
-                {
-                    cloneboard[x, y] = false;
-                }
-                else if (!gameBoard[x, y] && aliveNeighbors == 3)
-                {
-                    cloneboard[x, y] = true;
-                }
-                else
-                {
-                    cloneboard[x, y] = gameBoard[x, y];
-                }
-            }
-        }
-        gameBoard = cloneboard;
-    } 
+    public void SetValor(int x, int y, bool valor)
+    {
+        cells[x, y] = valor;
+    }
 }
     
