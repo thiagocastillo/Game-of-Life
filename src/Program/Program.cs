0b24e5f569d1bd606b58317c0fcca183c21fa92c
Santiago <st281306@fi365.ort.edu.uy>
2025-04-19 11:49:32 -0300

    generic filepath in program.cs test

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.IO;
 using Library;
 
 namespace Ucu.Poo.GameOfLife
@@ -9,10 +10,18 @@ namespace Ucu.Poo.GameOfLife
         static void Main() // Inicia el programa
         {
             // Crea un objeto de la clase BoardImporter para cargar el archivo board.txt
-            BoardImporter fileReader = new BoardImporter();
-            Board pantalla = fileReader.LoadBoardFromTxt("C:\\Users\\SANTIAGO\\Desktop\\UCU\\P2\\EJERCICIO GAME OF LIFE\\Game-of-Life\\assets\\board.txt");
+            //BoardImporter fileReader = new BoardImporter();
+            //Board pantalla = fileReader.LoadBoardFromTxt("C:\\Users\\SANTIAGO\\Desktop\\UCU\\P2\\EJERCICIO GAME OF LIFE\\Game-of-Life\\assets\\board.txt");
            //("C:\\Users\\Thiago Castillo\\Desktop\\Prog2\\Game-of-Life\\assets\\board.txt"); // Carga el tablero desde el archivo
             //("C:\Users\SANTIAGO\Desktop\UCU\P2\EJERCICIO GAME OF LIFE\Game-of-Life\assets\board.txt"); // Carga el tablero desde el archivo
+
+            string basePath = AppDomain.CurrentDomain.BaseDirectory;
+            string filePath = Path.Combine(basePath, "board.txt");
+            
+            
+            // Crea un objeto de la clase BoardImporter para cargar el archivo board.txt
+            BoardImporter fileReader = new BoardImporter();
+            Board pantalla = fileReader.LoadBoardFromTxt(filePath); // Carga el tablero desde el archivo
 
             
             while (true)
