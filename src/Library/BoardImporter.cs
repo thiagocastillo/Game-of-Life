e9f725fa14b9fedb8d42c0eb63de0bf43a16ace7
Santiago <st281306@fi365.ort.edu.uy>
2025-04-18 23:14:13 -0300

    BoardImporter.cs refactor, it seams that the rest of the classes works as expected.. ok..

--- src/Library/BoardImporter.cs
+++ src/Library/BoardImporter.cs
@@ -1,27 +1,27 @@
 using System;
 using System.IO;
+namespace Library;
 
-namespace Library
+public class BoardImporter  
 {
-    public class BoardImporter
+    public Board LoadBoardFromTxt(string filePath) //Metodo para cargar el tablero desde un archivo txt
     {
-        public Board LoadBoardFromTxt(string filePath)
-        {
-            string[] lines = File.ReadAllLines(filePath);
+        string[] lines = File.ReadAllLines(filePath); 
             
-            int rows = lines.Length;
-            int cols = lines[0].Trim().Length;
+        int rows = lines.Length;
+        int cols = lines[0].Trim().Length;
 
-            Board board = new Board(cols, rows); 
-            for (int i = 0; i < rows; i++)
+        Board board = new Board(cols, rows); 
+        
+        for (int i = 0; i < rows; i++)
+        {
+            string line = lines[i].Trim(); // Elimina espacios en blanco al inicio y al final de la linea
+            
+            for (int j = 0; j < cols; j++)
             {
-                string line = lines[i].Trim();
-                for (int j = 0; j < cols; j++)
-                {
-                    board.SetCell(j, i, line[j] == '1'); }
+                board.SetCell(j, i, line[j] == '1');  // Establece el estado de la célula en el tablero: '1' indica que está viva, '0' indica que está muerta
             }
-
-            return board;
         }
+        return board;
     }
 }
